7d7c094b520f41393e4eee0dfbc6a664033a0f17
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-02-03 00:42:13 +0900

    feat: api 커스텀 훅 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { useFetch } from './hooks';
 
 export default function App() {
-  return <div>hello</div>;
+  const { loading, data, error } = useFetch(process.env.REACT_APP_API_ROOT);
+  console.log(data);
+  if (loading) return <h1>Loading...</h1>;
+
+  if (error) console.log(error);
+
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
 }
---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+export function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetch = async () => {
+    try {
+      const response = await axios.get(url);
+      setData(response.data);
+      console.log(data);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    fetch();
+  }, [url]);
+
+  return { loading, data, error };
+}
+
+useFetch.propTypes = {
+  url: PropTypes.string.isRequired,
+};
